5682a4c8afd0ea626d80d9bdcf72de99507d44b8
SociallyAcceptable <123292694+SociallyAcceptable@users.noreply.github.com>
2024-12-29 09:29:13 +0800

    Update header.php
    changed the title

--- employee_crud/templates/header.php
+++ employee_crud/templates/header.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Employee Management</title>
+    <title>Horoscope</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="assets/bootstrap/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Employee Management</a>
+        <a class="navbar-brand" href="index.php">Horoscope</a>
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav ms-auto">
                 <?php if (isset($_SESSION['email'])): ?>
