7ec921ec292cbdfb7e6a7c817af5aa4bdfbd9567
SociallyAcceptable <123292694+SociallyAcceptable@users.noreply.github.com>
2024-12-29 09:29:32 +0800

    Update footer.php

--- employee_crud/templates/footer.php
+++ employee_crud/templates/footer.php
@@ -1,6 +1,6 @@
 </div>
     <footer class="bg-dark text-white text-center py-3 mt-4">
-        <p>&copy; 2024 CRUD Application by Maxine R. Sargento. All Rights Reserved.</p>
+        <p>&copy; 2024 Horoscope Application by Maxine Sargento and Maxine Nicole Landicho. All Rights Reserved.</p>
     </footer>
     <!-- Bootstrap JS Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
